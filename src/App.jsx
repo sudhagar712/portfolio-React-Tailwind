25c8e3def14334b4a0dd91062c95a6a5ddb4d277
sudhagar712 <sudhagarmsr712@gmail.com>
2024-11-09 13:43:21 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -50,11 +50,23 @@ const App = () => {
     <>
       {loading ? (
         <>
-          <div className="flex justify-center items-center h-screen relative">
-            {/* Loading spinner */}
-            <span class="loader"></span>
+          <div className="loading">
+            <div id="preloader">
+              <div class="spinner">
+                <span class="ball-1"></span>
+                <span class="ball-2"></span>
+                <span class="ball-3"></span>
+                <span class="ball-4"></span>
+                <span class="ball-5"></span>
+                <span class="ball-6"></span>
+                <span class="ball-7"></span>
+                <span class="ball-8"></span>
+              </div>
+              <h2 className="flex justify-center items-center h-screen font-extralight  text-3xl">
+                Loading...
+              </h2>
+            </div>
           </div>
-          
         </>
       ) : (
         <div className="overflow-x-hidden text-stone-300 antialiased">
