5390c4fed20087b0ac03f47970676fa1674875f1
sudhagar712 <sudhagarmsr712@gmail.com>
2025-06-05 21:38:18 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["emailjs-com"]
-    }
-  }
+  plugins: [react()]
+ 
 });
